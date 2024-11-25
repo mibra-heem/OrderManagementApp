1c3e7e3160c03a458605249340445ac3c01a6afc
mibra-heem <m.ibraheem4095332@gmail.com>
2024-11-25 12:49:05 +0500

    updated

--- lib/global.dart
+++ lib/global.dart
@@ -4,6 +4,8 @@ import 'package:flutter_developer_test/controller/product_controller.dart';
 import 'package:get/get.dart';
 class Global {
   static Future<void> init()async{
+    
+    
     Get.put(ProductController(apiClient: ApiClient(appBaseUrl: AppConstant.BASE_URL)));
   }
 }
